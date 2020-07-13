34b827851a0a38531e16ed282c8738e54ede174a
zan-tamaki <d-tamaki@studio-zan.com>
2020-07-13 18:45:50 +0900

    ポリゴン生成処理を更新

--- Runtime/Scripts/MeshCreator.cs
+++ Runtime/Scripts/MeshCreator.cs
@@ -14,36 +14,72 @@ namespace PolygonGenerator
 		 * @param vec_list	頂点座標のリスト
 		 * @param uv_list	UV座標のリスト
 		 */
-		 public void PolygonCreate( List<Vector3> vec_list, List<Vector2> uv_list)
-		 {
+		public void PolygonCreate( List<Vector3> vec_list, List<Vector2> uv_list)
+		{
 			int i0;
-			var mesh = new Mesh();
 			List<Vector3> vert = new List<Vector3>();
 			List<int> tri = new List<int>();
 			List<Vector2> uvs = new List<Vector2>();
-
-			/*! 頂点座標 */
-			mesh.SetVertices( vec_list);
+			List<Color32> color_list = new List<Color32>();
+			Color32 tmp_color;
 
 			/*! 頂点情報 */
 			for( i0 = 0; i0 < vec_list.Count; i0++)
 			{
 				tri.Add( i0);
 			}
-			mesh.SetTriangles( tri, 0);
 
-			mesh.SetUVs( 0, uv_list);
-			List<Color> color_list = new List<Color>();
-			Color tmp_color;
 			for( i0 = 0; i0 < vec_list.Count; i0++)
 			{
-				tmp_color = Color.red;
+				//tmp_color = new Color32(255,255,255,100);
+				tmp_color = new Color32(255,255,255,0);
+				color_list.Add(tmp_color);
 			}
-			mesh.SetColors( color_list);
+
+			PolygonCreate( vec_list, tri, uv_list, color_list);
+		}
+		
+		public void PolygonCreate( List<Vector3> vec_list)
+		{
+			int i0;
+			List<int> tri = new List<int>();
+			List<Vector2> uvs = new List<Vector2>();
+			Vector2 tmp_uv = Vector2.zero;
+			List<Color32> color_list = new List<Color32>();
+			Color32 tmp_color = new Color32(255,255,255,255);
+
+			/*! 頂点情報 */
+			for( i0 = 0; i0 < vec_list.Count; i0++)
+			{
+				tri.Add( i0);
+			}
+
+			/*! UV情報 */
+			for( i0 = 0; i0 < vec_list.Count; i0++)
+			{
+				uvs.Add( tmp_uv);
+			}
+
+			for( i0 = 0; i0 < vec_list.Count; i0++)
+			{
+				color_list.Add( tmp_color);
+			}
+			
+			PolygonCreate( vec_list, tri, uvs, color_list);
+		}
+
+		public void PolygonCreate( List<Vector3> vec_list, List<int> tri_list, List<Vector2> uv_list, List<Color32> col_list)
+		{
+			var mesh = new Mesh();
+
+			mesh.SetVertices( vec_list);
+			mesh.SetTriangles( tri_list, 0);
+			mesh.SetUVs( 0, uv_list);
+			mesh.SetColors( col_list);
 
 			mesh.RecalculateNormals();
 			var filter = GetComponent<MeshFilter>();
 			filter.sharedMesh = mesh;
-		 }
+		}
 	}
 }
