b0c84ce5163db5759de86258be3cd84ca185c6ac
zan-tamaki <d-tamaki@studio-zan.com>
2020-07-31 19:47:54 +0900

    UVの設定を修正

--- Runtime/Scripts/BuildingParameter.cs
+++ Runtime/Scripts/BuildingParameter.cs
@@ -66,7 +66,7 @@ namespace PolygonGenerator
 			Vector2[] vec_tbl = new Vector2[ 2];
 			float uv_x, uv_y;
 
-			var standardPoint = new Vector2( typeUv[ 0].x + 0.125f, typeUv[ 0].y);
+			var standardPoint = new Vector2( typeUv[ 3].x + 0.125f, typeUv[ 3].y);
 
 			switch( RoofTopType)
 			{
